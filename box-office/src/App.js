8e6327507d7face95176bf206ae602d98684c9e1
Anish10-debug <agbhat@mitaoe.ac.in>
2021-06-06 20:09:24 +0530

    edit react router

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+// here whatever goes inside the Route will be rendered at that path
+// this means that, the content of Route will be rendered only if
+// the url matches the path given in Route
+// for undefined paths we dont put exact path attribute
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>This is 404</Route>
+    </Switch>
+  );
 }
 
 export default App;
--- box-office/src/index.js
+++ box-office/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// here browser router is used because we will be having multiple pages
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // eslint-disable-next-line comma-dangle
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
