456a73582924d528c88b6500240ff9bd0ffb3b3a
Anish10-debug <agbhat@mitaoe.ac.in>
2021-06-06 20:38:20 +0530

    added navbar and created page components

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 // here whatever goes inside the Route will be rendered at that path
 // this means that, the content of Route will be rendered only if
 // the url matches the path given in Route
 // for undefined paths we dont put exact path attribute
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>This is 404</Route>
-    </Switch>
+        <Route>This is 404 NOT FOUND</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ box-office/src/components/Navs.js
@@ -0,0 +1,27 @@
+/*eslint-disable*/
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LINKS = [
+  //here an array of objects is created which contains the link
+  { to: '/', text: 'Home' },
+  { to: '/starred', text: 'Starred' },
+];
+
+const Navs = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          {LINKS.map(item => (
+            <li key={item.to}>
+              <Link to={item.to}>{item.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navs;
