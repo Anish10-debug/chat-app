23f60f5967a164ffa16c8028e7e074bc6d831b74
Anish10-debug <agbhat@mitaoe.ac.in>
2021-06-16 12:12:00 +0530

    created reusable and editable input

---
+++ chat-app/src/components/dashboard/EditableInput.js
@@ -0,0 +1,63 @@
+/*eslint-disable*/
+import React, { useCallback, useState } from 'react';
+import { Icon, Input, InputGroup, Button, Alert } from 'rsuite';
+const EditableInput = ({
+  initialValue,
+  onSave,
+  label = null,
+  placeholder = 'Write your value',
+  emptyMsg = 'Input is empty',
+  ...inputProps
+}) => {
+  const [input, setInput] = useState(initialValue); //allow the user to change the value
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const [isEditable, setIsEditable] = useState(false); //input will be disabled when isEditable id false
+  //close and edit2 are the names of the icon from the rsuite library
+
+  const onEditClick = useCallback(() => {
+    setIsEditable(p => !p); //change to true and then we can edit
+    setInput(initialValue); //if we wish to cancel editing then our current profile name will be displayed
+  }, [initialValue]);
+
+  const onSaveClick = async () => {
+    const trimmed = input.trim(); //to remove unnecessary spaces on our editted value
+    if (trimmed === '') {
+      Alert.info(emptyMsg, 4000); //if the input box is empty and we try to save
+    }
+
+    if (trimmed != initialValue) {
+      await onSave(trimmed); //if the new name and previous name dont match i.e if there is some change
+      //we pass this trimmed value to onSave function
+      Alert.info('Changes have been made', 4000);
+    }
+
+    setIsEditable(false);
+  };
+  return (
+    <div>
+      {label}{' '}
+      <InputGroup>
+        <Input
+          {...inputProps}
+          disabled={!isEditable}
+          placeholder={placeholder}
+          value={input}
+          onChange={onInputChange}
+        />
+        <InputGroup.Button onClick={onEditClick}>
+          <Icon icon={isEditable ? 'close' : 'edit2'} />
+        </InputGroup.Button>
+        {isEditable && (
+          <InputGroup.Button onClick={onSaveClick}>
+            <Icon icon="check" />
+          </InputGroup.Button>
+        )}
+      </InputGroup>
+    </div>
+  );
+};
+
+export default EditableInput;
--- chat-app/src/components/dashboard/index.js
+++ chat-app/src/components/dashboard/index.js
@@ -1,9 +1,14 @@
 /*eslint-disable*/
 import React from 'react';
-import { Icon, Button, Drawer } from 'rsuite';
+import { Icon, Button, Drawer, Divider } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from './EditableInput';
 
+//Here we will style the drawer and also provide options to change profile name and sign out
 const Dashboard = ({ onSignout }) => {
+  const onSave = async newData => {
+    console.log(newData);
+  };
   const { profile } = useProfile();
   return (
     <>
@@ -13,6 +18,12 @@ const Dashboard = ({ onSignout }) => {
 
       <Drawer.Body>
         <h3> Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
 
       <Drawer.Footer>
